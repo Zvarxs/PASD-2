c51405305e2dddb54bc23826bd2530a8044a634b
Zvarxs <162097128+Zvarxs@users.noreply.github.com>
2024-04-02 03:02:13 +0700

    Add files via upload

--- Unguided1.cpp
+++ Unguided1.cpp
@@ -1,51 +1,32 @@
 #include <iostream>
-#include <vector>
+
+void selectionSort(double arr[], int n) {
+    for (int i = 0; i < n - 1; ++i) {
+        int max_idx = i;
+        for (int j = i + 1; j < n; ++j) {
+            if (arr[j] > arr[max_idx]) {
+                max_idx = j;
+            }
+        }
+        std::swap(arr[i], arr[max_idx]);
+    }
+}
 
 int main() {
-    // Mendeklarasikan variabel dan vektor
-    int num;
-    std::vector<int> dataArray;
-    std::vector<int> evenNumbers;
-    std::vector<int> oddNumbers;
+    const int n = 5;
+    double ips[n] = {3.8, 2.9, 3.3, 4.0, 2.4};
 
-    // Input data array dari pengguna
-    std::cout << "Masukkan 10 angka: ";
-    for (int i = 0; i < 10; ++i) {
-        std::cin >> num;
-        dataArray.push_back(num);
-    }
-
-    // Memisahkan angka genap dan ganjil
-    for (int i = 0; i < 10; ++i) {
-        if (dataArray[i] % 2 == 0) {
-            evenNumbers.push_back(dataArray[i]);
-        } else {
-            oddNumbers.push_back(dataArray[i]);
-        }
-    }
-
-    // Menampilkan output
-    std::cout << "Data Array: ";
-    for (int i = 0; i < 10; ++i) {
-        std::cout << dataArray[i] << " ";
+    std::cout << "IPS sebelum diurutkan: ";
+    for (int i = 0; i < n; ++i) {
+        std::cout << ips[i] << " ";
     }
     std::cout << std::endl;
 
-    std::cout << "Nomor Genap: ";
-    for (int i = 0; i < evenNumbers.size(); ++i) {
-        std::cout << evenNumbers[i];
-        if (i != evenNumbers.size() - 1) {
-            std::cout << ", ";
-        }
-    }
-    std::cout << std::endl;
+    selectionSort(ips, n);
 
-    std::cout << "Nomor Ganjil: ";
-    for (int i = 0; i < oddNumbers.size(); ++i) {
-        std::cout << oddNumbers[i];
-        if (i != oddNumbers.size() - 1) {
-            std::cout << ", ";
-        }
+    std::cout << "IPS setelah diurutkan dari terbesar hingga terkecil: ";
+    for (int i = 0; i < n; ++i) {
+        std::cout << ips[i] << " ";
     }
     std::cout << std::endl;
 

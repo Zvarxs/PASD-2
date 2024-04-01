d96a5d724f8d7ed92812df48ce1bead432765b9c
Zvarxs <162097128+Zvarxs@users.noreply.github.com>
2024-04-02 03:00:07 +0700

    Add files via upload

--- Guided1.cpp
+++ Guided1.cpp
@@ -1,48 +1,39 @@
 #include <iostream>
 using namespace std;
-// PROGRAM INPUT ARRAY 3 DIMENSI
-int main()
-{
-// Deklarasi array
-int arr[2][3][3];
-// Input elemen
-for (int x = 0; x < 2; x++)
-{
-for (int y = 0; y < 3; y++)
-{
-for (int z = 0; z < 3; z++)
-{
-cout << "Input Array[" << x << "][" << y << "][" << z <<
-"] = ";
-cin >> arr[x][y][z];
+
+void bubble_sort(double arr[], int length){
+    bool not_sorted = true;
+    int j=0;
+    double tap;
+
+    while (not_sorted){
+        not_sorted = false;
+        j++;
+        for(int i = 0; i < length - j; j++){
+            if(arr[i] > arr[i + 1]){
+                tap = arr[i];
+                arr[i] = arr[i +1];
+                arr[i + 1] = tap;
+                not_sorted = true;
+            }// end off it 
+        }// end for loop
+        
+    }// end of while loop
+}// end of buble short 
+
+void print_array(double a[], int length) {
+
+    for(int i=0; i < length; i++){
+        cout << a[i] << "\t";
+    }
+    cout << endl;
 }
-}
-cout << endl;
-}
-// Output Array
-for (int x = 0; x < 2; x++)
-{
-for (int y = 0; y < 3; y++)
-{
-for (int z = 0; z < 3; z++)
-{
-cout << "Data Array[" << x << "][" << y << "][" << z <<
-"] = " << arr[x][y][z] << endl;
-}
-}
-}
-cout << endl;
-// Tampilan array
-for (int x = 0; x < 2; x++)
-{
-for (int y = 0; y < 3; y++)
-{
-for (int z = 0; z < 3; z++)
-{
-cout << arr[x][y][z] << ends;
-}
-cout << endl;
-}
-cout << endl;
-}
+int main(){
+    int length = 4;
+    double a[] = {15.7, 87.3, 7.5, 88.5, 67.9};
+    cout << "urutan bilangan sebelum sorting : " << endl;
+    print_array(a, length);
+    bubble_sort(a, length);
+    cout << "\n urutan bilangan setelah sortiing: " << endl;
+    print_array(a, length);
 }

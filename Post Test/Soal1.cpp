f7237f2f8d96ea6d7e5540a2d7e9769c61727c21
Zvarxs <162097128+Zvarxs@users.noreply.github.com>
2024-06-10 14:24:52 +0700

    Add files via upload

---
+++ Post Test/Soal1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+main {
+
+    Data = (1, 2, 3, 4, 5, 5){
+        if min (1) =/ 5
+        if else (2, 3, 4) =/ 5
+        else (5) = 5
+    }
+}
+if else main data
+return 0;

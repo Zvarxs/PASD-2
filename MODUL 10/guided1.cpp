6ab6d309bcb9e336576d0e3f02218037d2195842
Zvarxs <162097128+Zvarxs@users.noreply.github.com>
2024-06-15 13:21:48 +0700

    Update guided1.cpp

---
+++ MODUL 10/guided1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int faktorial(int n) {
+    if (n == 0 || n == 1)
+        return 1;
+    else
+        return n * faktorial(n - 1);
+}
+
+int main() {
+    int bilangan;
+
+    cout << "Masukkan bilangan bulat positif: ";
+    cin >> bilangan;
+
+    if (bilangan < 0) {
+        cout << "Harap masukkan bilangan bulat positif." << endl;
+    } else {
+        int hasil = faktorial(bilangan);
+        cout << "Faktorial dari " << bilangan << " adalah: " << hasil << endl;
+    }
+
+    return 0;
+}
